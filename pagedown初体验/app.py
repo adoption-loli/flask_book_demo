4cfdefcdf4490adecbff7e47ca4c90f80cdea1c4
adoption-loli <1509636344@qq.com>
2019-05-27 22:19:25 +0800

    pagedown再体验

--- pagedown初体验/app.py
+++ pagedown初体验/app.py
@@ -1,9 +1,12 @@
-from flask import Flask,render_template
+from flask import Flask, render_template, redirect, url_for, flash, request
 from flask_pagedown import PageDown
-from flask_wtf import Form
+from flask_wtf import FlaskForm
 from wtforms.validators import required
 from flask_pagedown.fields import PageDownField
 from wtforms.fields import SubmitField
+from markdown import markdown
+import bleach
+from flask_sqlalchemy import SQLAlchemy
 
 
 pagedown = PageDown()
@@ -12,15 +15,22 @@ pagedown.init_app(app)
 app.config["SECRET_KEY"] = "12345678"
 
 
-class PostForm(Form):
-    body = PageDownField("Post:", validators=[required()])
+class PostForm(FlaskForm):
+    body = PageDownField("Post:")
     submit = SubmitField("Submit")
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def hello_world():
-    form=PostForm()
-    return render_template('new_post.html', form=form)
+    forms = PostForm()
+    return render_template('new_post.html', form=forms)
+
+
+@app.route('/print', methods=['POST'])
+def print123():
+    form = PostForm(request.form)
+    print(str(form.body.data))
+    return 'hhh'
 
 
 if __name__ == '__main__':
